9612d8605304c3face705390468fb556ac32a46f
Thanh Khan <k.coder.282@gmail.com>
2022-02-15 22:57:41 +0700

    create app

---
+++ teaching_manager/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route/>    
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
